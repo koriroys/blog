de84ceda6b9a037a9c5acec340183acc595aa98e
Kori Roys <kori@koriroys.com>
2012-01-06 08:49:26 -0600

    New blog structure, no styling yet

---
+++ help/raphael_demos/dot.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  // Canvas & Border
+
+  const paperWidth = 100;
+  const paperHeight = 100;
+  const maxRadius = 10;
+
+  randRange = function(M, N) {
+    return Math.floor(M + (1+N-M)*Math.random());
+  };
+
+
+  var paper = Raphael(10, 40, paperWidth, paperHeight);
+  var border = paper.rect(0, 0, paperWidth, paperHeight).attr({'stroke-width': 1});
+
+  // Template for the dots
+  var dot = paper.circle(paperWidth/2, paperHeight/2, 0).attr({fill: "#000"});
+
+  // Making the button work
+  $("#button1").click(function(){
+    var dotClone = dot.clone();
+
+
+
+    var randomNumberR = randRange(0, maxRadius);  
+    // Ensures that all circles draw within the borders of the paper.
+    var randomNumberX = randRange(maxRadius + border.attr('stroke-width'), paperWidth - maxRadius - border.attr('stroke-width')); 
+    var randomNumberY = randRange(maxRadius + border.attr('stroke-width'), paperHeight - maxRadius- border.attr('stroke-width'));
+
+    dotClone.animate({cx:randomNumberX, cy: randomNumberY, r:randomNumberR}, 1000);	
+  })
+});
---
+++ toys.markdown
@@ -0,0 +1,23 @@
+---
+layout: default
+title: My projects
+class: projects
+---
+
+<dl>
+  <dt>Raphael Sample 1</dt>
+	<dd><p> <a href="/raphael_demos/colored_circle.html">Coloring book test</a></p>
+	</dd>
+	
+	<dt>Raphael Sample 2</dt>
+	<dd><p> <a href="/raphael_demos/dot.html">A bunch of flying dots</a></p>
+	</dd>
+	
+	<dt>Raphael Sample 3</dt>
+	<dd><p> <a href="/raphael_demos/rectangle.html">A rectangle</a></p>
+	</dd>
+	
+	<dt>Raphael Sample 4</dt>
+	<dd><p> <a href="/raphael_demos/trojan_victory.html">Team banner</a></p>
+	</dd>
+</dl>
